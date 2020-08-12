68363ae50cec16e41f75f0d63178ff3e2557d8a8
phongcolongchong <57311841+phongcolongchong@users.noreply.github.com>
2020-08-12 13:00:42 +0700

    [Add] SidebarItem component

--- tranquangphong/component/src/Main/index.jsx
+++ tranquangphong/component/src/Main/index.jsx
@@ -4,10 +4,29 @@ import List from './List';
 import './styles.css';
 
 function Main() {
+  const dataList = [
+    {
+      name: 'Nguyen Van A',
+      color: '#274b69',
+    },
+    {
+      name: 'Tran Van B',
+      color: '#85a1c1',
+    },
+    {
+      name: 'Ly Thi C',
+      color: '#c6ccd8',
+    },
+    {
+      name: 'Do Quang D',
+      color: '#3f4d63',
+    }
+  ];
+
   return (
     <div className="main">
       <Sidebar />
-      <List />
+      <List dataList={dataList} />
     </div>
   );
 };
