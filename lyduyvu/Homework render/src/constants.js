4311bd1052949bb270b594f967bd7ba30a376cee
duyvu0302 <64111862+duyvu0302@users.noreply.github.com>
2020-08-16 07:37:55 +0700

    [Add] file homework

---
+++ lyduyvu/Homework render/src/constants.js
@@ -0,0 +1,76 @@
+const groupList = [
+  {
+    name: 'Group 1',
+    description: 'asdosando asdpams dmsa asd a',
+    members: [
+      {
+        name: 'Lê Bá Thanh Tuấn',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'ABC',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'XYZ',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'XYZ',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'XYZ',
+        image: 'https://via.placeholder.com/100',
+      },
+    ],
+  },
+  {
+    name: 'Group 2',
+    description: 'asdosando asdpams dmsa asd a',
+    members: [
+      {
+        name: 'Lê Bá Thanh Tuấn',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'ABC',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'XYZ',
+        image: 'https://via.placeholder.com/100',
+      },
+    ],
+  },
+  {
+    name: 'Group 3',
+    description: 'asdosando asdpams dmsa asd a',
+    members: [
+      {
+        name: 'Lê Bá Thanh Tuấn',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'ABC',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'XYZ',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'XYZ',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'XYZ',
+        image: 'https://via.placeholder.com/100',
+      },
+      {
+        name: 'XYZ',
+        image: 'https://via.placeholder.com/100',
+      },
+    ],
+  }
+];
