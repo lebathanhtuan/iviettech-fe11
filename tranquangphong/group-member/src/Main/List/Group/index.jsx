524a57f4ae32f6f29c093b699ef0849ab4f61354
phongcolongchong <57311841+phongcolongchong@users.noreply.github.com>
2020-08-18 15:52:53 +0700

    [Add] feature select and delete group members

--- tranquangphong/group-member/src/Main/List/Group/index.jsx
+++ tranquangphong/group-member/src/Main/List/Group/index.jsx
@@ -3,19 +3,24 @@ import './styles.css';
 
 function Group(props) {
   const { 
+    id,
     name, 
     description, 
     members, 
     activeIndex,
     activeGroup,
-    setActiveGroup
-   } = props;
-
+    setActiveGroup,
+    cartData,
+    setCartData,
+    groupItem,
+    isCart
+  } = props;
+  
   const renderMembers = () => {
     return members.map((member, index) => {
       if (index < 3) {
         return (
-          <img key={index} className="image" src={ member.image } alt="image"/>   
+          <img key={index} className="image" src={ member.image } alt="member"/>   
         );
       } else {
         return null;
@@ -25,7 +30,6 @@ function Group(props) {
 
   const renderRemainder = () => {
     let numOfRemainder = members.length - 3
-
     if (numOfRemainder > 0) {
       return (
         <div className="container-remainder">
@@ -36,12 +40,37 @@ function Group(props) {
     }
   }
 
+  const handleSelectGroup = () => {
+    setActiveGroup(activeIndex)
+    const cartIndex = cartData.findIndex((data) => data.id === groupItem.id);
+    if (cartIndex === -1) {
+      setCartData([
+        ...cartData,
+        groupItem
+      ]);      
+    }
+  }
+
+  const handleDeleteGroup = () => {
+    const cartIndex = cartData.findIndex((data) => data.id === groupItem.id);
+    cartData.splice(cartIndex, 1);
+    setCartData([...cartData]);
+  }
+
   return (
     <div className="col-4">
       <div className={`item ${activeIndex === activeGroup ? "active" : ""}`}>
         <div className="d-flex my-3">
           <div className="font-weight-bold mx-3 d-flex align-items-center">{ name }</div>
-          <button className="btn btn-success btn-sm" onClick={() => setActiveGroup(activeIndex)}>Select</button>
+          {isCart ? (
+            <button 
+              className="btn btn-danger btn-sm" 
+              onClick={() => handleDeleteGroup()}>Delete
+            </button>) : (
+            <button 
+              className="btn btn-success btn-sm" 
+              onClick={() => handleSelectGroup()}>Select
+            </button>)}
         </div>
         <div className="container-image">{ renderMembers() } { renderRemainder() }</div>
         <div className="border-top px-3 py-2 bg-light">{ description }</div>
--- tranquangphong/group-member/src/Main/List/index.jsx
+++ tranquangphong/group-member/src/Main/List/index.jsx
@@ -5,18 +5,43 @@ import './styles.css';
 function List(props) {
   const { groupList } = props;
   const [ activeGroup, setActiveGroup ] = useState(0);
+  const [ cartData, setCartData ] = useState([]);
 
   const renderGroups = () => {
     return groupList.map((item, index) => {
       return (
         <Group 
         key={index} 
+        id={item.id} 
         name={item.name} 
         description={item.description} 
         members={item.members}
         activeIndex={index}
         activeGroup={activeGroup}
         setActiveGroup={setActiveGroup}
+        cartData={cartData}
+        setCartData={setCartData}
+        groupItem={item}
+        />
+      );
+    });
+  }
+
+  const renderCart = () => {
+    return cartData.map((item, index) => {
+      return (
+        <Group 
+        key={index}
+        name={item.name} 
+        description={item.description} 
+        members={item.members}
+        activeIndex={index}
+        activeGroup={activeGroup}
+        setActiveGroup={setActiveGroup}
+        cartData={cartData}
+        setCartData={setCartData}
+        groupItem={item}
+        isCart
         />
       );
     });
@@ -24,6 +49,10 @@ function List(props) {
 
   return (
     <div className="list">
+      <p className="title">Cart</p>
+      <div className="row px-3">
+        {renderCart()}
+      </div>
       <p className="title">Groups list</p>
       <div className="row px-3">
         {renderGroups()}
